214c12fdb09b1a1b713b3231ed63880b8a89198f
menago <piszczek.wojciech@o2.pl>
2025-07-17 20:09:25 +0200

    Update oauth.js

--- test/api/oauth.js
+++ test/api/oauth.js
@@ -7,7 +7,7 @@ const supabase = createClient(
 );
 
 const guildId = '1395463783215796224'; // <- ID twojego serwera
-const botToken = process.env.DISCORD_BOT_TOKEN; // <- Dodaj do .env lub wpisz ręcznie (jeśli testujesz)
+const botToken = process.env.1395464662727786597; // <- Dodaj do .env lub wpisz ręcznie (jeśli testujesz)
 
 export default async function handler(req, res) {
   const code = req.query.code;
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
 
     // 3. Pobranie ról użytkownika z serwera
     const memberRes = await fetch(`https://discord.com/api/guilds/${guildId}/members/${id}`, {
-      headers: { Authorization: `Bot ${botToken}` }  // Użyj tokena BOTA
+      headers: { Authorization: `Bot ${MTM5NTQ2NDY2MjcyNzc4NjU5Nw.GsxDRY.5lI_EXodVlAVfF8Hg7GGIflVOvi2RGxxRakBTo}` }  // Użyj tokena BOTA
     });
 
     if (!memberRes.ok) {
